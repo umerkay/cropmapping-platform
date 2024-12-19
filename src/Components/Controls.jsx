62182ba596bd8995f1dec6590e9323b1d45337a6
umerkay <umerkk164@gmail.com>
2024-12-19 17:08:11 +0500

    added districts

--- src/Components/Controls.jsx
+++ src/Components/Controls.jsx
@@ -1,93 +1,16 @@
-import { Check, Download, Map, RectangleHorizontalIcon, Stars, X } from "lucide-react";
-import React, { useEffect, useState } from "react";
-import { getTileInfo, getTimestamps, generateMap } from "../Services/TileService"; // import generateMap
 import { DarkModeSwitch } from 'react-toggle-dark-mode';
-import ToggleControl from "./ToggleControl";
+import React, { useState } from "react";
 
-const fetchTimestamps = async (setTimestamps) => {
-    const timestamps = await getTimestamps();
-    setTimestamps(timestamps);
-};
-
-function Controls({ mousePosition, rectangleBounds, resetRect, setTiles, mode, setMode, setMetaData }) {
-
-    const [timestamps, setTimestamps] = useState([]);
-    const [isGenerating, setIsGenerating] = useState(false); // state to track if the map is being generated
-
+function Controls({ polygons, onPolygonSelect }) {
     const [selected, setSelected] = useState({
         timestamp: "",
-        // month: "",  // commented out month
-        // year: "",   // commented out year
         showMap: true
     });
 
-    useEffect(() => {
-        fetchTimestamps(setTimestamps);
-    }, []);
+    const mode = "dark"; // mode is always dark
 
-    const fetchTiles = async () => {
-        const data = await getTileInfo(selected.timestamp);
-        setTiles(data.tiles);
-        setMetaData(
-            {
-                name: data.name,
-                description: data.description,
-                model: data.model,
-                source: data.source
-            }
-        )
-    };
-
-    useEffect(() => {
-        setTiles([]);
-        setMetaData({});
-    }, [selected.timestamp]);
-
-    const select = (e) => {
-        setSelected(
-            {
-                ...selected,
-                [e.target.name]: e.target.value
-            }
-        )
-    }
-
-    useEffect(() => {
-        setMetaData(
-            (prev) => {
-                return {
-                    ...prev,
-                    showMap: selected.showMap
-                }
-            }
-        )
-    }, [selected.showMap]);
-
-    // Function to handle map generation
-    const handleGenerateMap = async () => {
-        if (!rectangleBounds) return;
-
-        setIsGenerating(true);  // start loading state
-
-        try {
-            const boundingBox = {
-                min_lat: rectangleBounds.south,
-                min_lon: rectangleBounds.west,
-                max_lat: rectangleBounds.north,
-                max_lon: rectangleBounds.east
-            };
-
-            const data = await generateMap(boundingBox);
-            console.log('Map generated successfully:', data);
-            fetchTimestamps(setTimestamps);
-
-            // You can update the UI based on the generated map response here
-        } catch (error) {
-            console.error('Error generating map:', error);
-        } finally {
-            setIsGenerating(false);  // end loading state
-        }
-    };
+    // Sort the districts alphabetically by the district name
+    const sortedPolygons = polygons.sort((a, b) => a.name.localeCompare(b.name));
 
     return (
         <div className="controls">
@@ -99,92 +22,15 @@ function Controls({ mousePosition, rectangleBounds, resetRect, setTiles, mode, s
                 <h3>AgroData PK</h3>
             </div>
             <div className="content">
-                <p>
-                    Lat: {mousePosition.lat.toFixed(4)}, Lng: {mousePosition.lng.toFixed(4)}
-                </p>
-                <h3>Load</h3>
-                <label htmlFor="timestamp" className="label">Available Maps:</label>
-                <select name="timestamp" id="timestamp" onChange={select}>
-                    <option value="">Select</option>
-                    {
-                        timestamps.map((timestamp, i) => (
-                            <option key={i} value={timestamp}>{timestamp}</option>
-                        ))
-                    }
+                <h3>Select District</h3>
+                <select onChange={(e) => onPolygonSelect(e.target.value)}>
+                    <option value="">Select a district</option>
+                    {sortedPolygons.map((polygon, index) => (
+                        <option key={index} value={polygon.id}>
+                            {polygon.name}
+                        </option>
+                    ))}
                 </select>
-                <button
-                    onClick={fetchTiles}
-                    disabled={!selected.timestamp}
-                >
-                    <Map />
-                    Load Map
-                </button>
-                {
-                    true &&
-                    (
-                        <ToggleControl
-                            label="Show Overlay"
-                            value={selected.showMap}
-                            toggle={(e) => setSelected({ ...selected, showMap: !selected.showMap })}
-                        />
-                    )
-                }
-                <h3>Generate</h3>
-
-                {/* Commented out the month and year selection */}
-                {/* 
-                <label htmlFor="month" className="label">Select Month:</label>
-                <select name="month" id="month" onChange={select}>
-                    <option value="1">January</option>
-                    <option value="2">February</option>
-                    <option value="3">March</option>
-                    <option value="4">April</option>
-                    <option value="5">May</option>
-                    <option value="6">June</option>
-                    <option value="7">July</option>
-                    <option value="8">August</option>
-                    <option value="9">September</option>
-                    <option value="10">October</option>
-                    <option value="11">November</option>
-                    <option value="12">December</option>
-                </select>
-                <label htmlFor="year" className="label">Select Year:</label>
-                <select name="year" id="year" onChange={select}>
-                    <option value="2020">2020</option>
-                    <option value="2021">2021</option>
-                    <option value="2022">2022</option>
-                    <option value="2023">2023</option>
-                    <option value="2024">2024</option>
-                </select>
-                */}
-
-                <button
-                    onClick={handleGenerateMap}
-                    disabled={!rectangleBounds || isGenerating}  // disable if no bounds or generating
-                >
-                    {isGenerating ? <span>Generating...</span> : <><Stars /> Generate Map</>}
-                </button>
-
-                {
-                    rectangleBounds ? (<>
-                        <h3>Bounding Box</h3>
-
-                        <div className="bounds" style={{ textAlign: "left" }}>
-                            <p>North East: {rectangleBounds?.north}, {rectangleBounds?.east}</p>
-                            <p>South West: {rectangleBounds?.south}, {rectangleBounds?.west}</p>
-                        </div>
-                        <button disabled={!rectangleBounds} onClick={resetRect}>
-                            <RectangleHorizontalIcon />
-                            Reset Bounds
-                        </button>
-                        <button disabled={!rectangleBounds}>
-                            <Download />
-                            Download Labels
-                        </button>
-
-                    </>
-                    ) : null
-                }
             </div>
             <div className="footer">
                 <img className="machvislogo" src={mode === "dark" ? "machvislogo.png" : "machvislogolight.png"} alt="logo" />
--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,14 +1,10 @@
-import React, { useState, useRef, useCallback, useEffect } from 'react';
-import { GoogleMap, LoadScript, Rectangle } from '@react-google-maps/api';
+import React, { useState, useRef, useEffect } from 'react';
+import { GoogleMap, LoadScript, Polygon } from '@react-google-maps/api';
 import Controls from './Controls';
-import Insights from './Insights';
-import { getTimestamps } from '../Services/TileService';
 
 const containerStyle = {
     width: '100%',
     height: '100%',
-    // borderRadius: '1rem',
-    // border: "5px solid rgb(17, 124, 206)"
 };
 
 const center = {
@@ -16,164 +12,76 @@ const center = {
     lng: 70,
 };
 
-const rectangleOptions = {
+const polygonOptions = {
     fillColor: '#FF0000',
     fillOpacity: 0,
     strokeColor: '#FF0000',
     strokeOpacity: 0.8,
     strokeWeight: 2,
+    strokeDasharray: [4, 4] // Makes the border dotted (4px dash, 4px space)
 };
 
-// const imgs = ["/output_image.png"]
-// const imageBounds = [{
-//     north: 28.021667455423074,  // Top latitude (from conversion)
-//     south: 27.034136567742728,  // Bottom latitude (from conversion)
-//     east: 69.09840173726721,   // Right longitude (from conversion)
-//     west: 67.9823586865616,   // Left longitude (from conversion), 
-// }]
-
-const libraries = ['drawing'];
-
 function App({ mode, setMode }) {
-    const [rectangleBounds, setRectangleBounds] = useState(null);
-    const [rectangle, setRectangle] = useState(null);
+    const [polygons, setPolygons] = useState([]);
+    const [selectedPolygon, setSelectedPolygon] = useState(null);
+    const [geojsonData, setGeojsonData] = useState(null);
     const mapRef = useRef(null);
-    const drawingManagerRef = useRef(null);
-    const [tiles, setTiles] = useState([]);
-    const [metaData, setMetaData] = useState({});
-    const [overlays, setOverlays] = useState([]);
-    const [mousePosition, setMousePosition] = useState({ lat: 0, lng: 0 });
 
-    const onLoad = useCallback((map) => {
-        mapRef.current = map;
-
-        drawingManagerRef.current = new window.google.maps.drawing.DrawingManager({
-            drawingMode: null,
-            drawingControl: true,
-            drawingControlOptions: {
-                drawingModes: [
-                    window.google.maps.drawing.OverlayType.RECTANGLE,
-                ],
-                position: window.google.maps.ControlPosition.TOP_CENTER,
-            },
-            rectangleOptions: rectangleOptions,
-        });
-
-        drawingManagerRef.current.setMap(map);
-
-        // Handle rectangle drawing
-        window.google.maps.event.addListener(drawingManagerRef.current, 'rectanglecomplete', (rectangle) => {
-            setRectangle(rectangle);
-            const bounds = rectangle.getBounds();
-            setRectangleBounds(bounds.toJSON());
-            drawingManagerRef.current.setMap(null); // Hide drawing manager after drawing
-        });
-
-        window.google.maps.event.addListener(map, "mousemove", function (event) {
-            // Get latitude and longitude of the mouse position
-            var lat = event.latLng.lat();
-            var lng = event.latLng.lng();
-            setMousePosition({ lat, lng });
-        });
-
+    // Fetch the GeoJSON data and list of polygons
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_API_URL}api/geojson`)
+            .then(response => response.json())
+            .then(data => {
+                setPolygons(data.polygons);
+                setGeojsonData(data.geojson);
+            });
     }, []);
 
-    const resetRect = () => {
-        setRectangleBounds(null); // Clear the rectangle bounds
-        rectangle.setMap(null); // Clear the rectangle
-        setRectangle(null); // Clear the rectangle state
-        if (drawingManagerRef.current) {
-            drawingManagerRef.current.setMap(mapRef.current); // Show drawing manager again
-        }
+    // Handle polygon selection
+    const handlePolygonSelect = (polygonId) => {
+        const selected = geojsonData.features.find(feature => feature.properties.GID_3 === polygonId);
+        console.log(selected.geometry.coordinates);
+        setSelectedPolygon(selected);
     };
 
-    useEffect(() => {
-        if (overlays.length > 0) {
-            overlays.forEach((overlay) => {
-                overlay.setMap(null);
-            });
-        }
-        setOverlays([]);
+    const convertCoordinates = (coordinates) => {
+        return coordinates[0].map(([lon, lat]) => ({ lat, lng: lon })); // Flip coordinates
+    };
 
-        // Load image overlay
-        const loadOverlay = async () => {
-            for (let tile of tiles) {
-                const imageUrl = `${import.meta.env.VITE_API_URL}map/data/${metaData.name}/${tile[0]}`;
-
-                // Fetch the image with custom header
-                const response = await fetch(imageUrl, {
-                    method: 'GET',
-                    headers: {
-                        'ngrok-skip-browser-warning': 'true' // Custom header to bypass the warning
-                    }
-                });
-
-                if (response.ok) {
-                    // Create blob URL from response
-                    const imageBlob = await response.blob();
-                    const imageObjectURL = URL.createObjectURL(imageBlob);
-
-                    console.log('Image loaded:', imageObjectURL);
-
-                    // Create the GroundOverlay with the blob URL
-                    const overlay = new window.google.maps.GroundOverlay(
-                        imageObjectURL, // Use blob URL
-                        {
-                            north: tile[1],
-                            south: tile[2],
-                            east: tile[3],
-                            west: tile[4]
-                        }
-                    );
-                    overlay.setMap(mapRef.current);
-                    setOverlays((prev) => [...prev, overlay]);
-                } else {
-                    console.error('Failed to load image overlay');
-                }
-            }
-        };
-
-        loadOverlay();
-    }, [tiles]);
-
-
-    useEffect(() => {
-        if (metaData.showMap === false) {
-            if (overlays.length > 0) {
-                overlays.forEach((overlay) => {
-                    overlay.setMap(null);
-                });
-            }
-        } else if (metaData.showMap === true) {
-            if (overlays.length > 0) {
-                overlays.forEach((overlay) => {
-                    overlay.setMap(mapRef.current);
-                });
-            }
-        }
-    }, [metaData.showMap]);
+    const flattenCoordinates = (coords) => {
+        return coords.reduce((acc, val) => {
+          if (Array.isArray(val)) {
+            // If the array is nested, recursively flatten
+            return acc.concat(flattenCoordinates(val)); 
+          } else if (Array.isArray(val) && val.length === 2) {
+            // If it's a valid coordinate pair, push it to the result
+            return acc.concat([val]);
+          } else {
+            // Otherwise, ignore invalid values
+            console.error('Invalid coordinate structure:', val);
+            return acc;
+          }
+        }, []);
+      };
 
     return (
-        <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS} libraries={libraries}>
-            <Controls mousePosition={mousePosition} setMetaData={setMetaData} mode={mode} setMode={setMode} setTiles={setTiles} rectangleBounds={rectangleBounds} resetRect={resetRect} />
+        <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS}>
+            <Controls polygons={polygons} onPolygonSelect={handlePolygonSelect} />
             <div id='map'>
                 <GoogleMap
                     mapContainerStyle={containerStyle}
                     center={center}
                     zoom={6}
-                    onLoad={onLoad}
-                    options={{ streetViewControl: false }}
+                    onLoad={(map) => { mapRef.current = map; }}
                 >
-                    {/* Render the rectangle only if bounds exist */}
-                    {rectangleBounds && (
-                        <Rectangle
-                            bounds={rectangleBounds}
-                            options={rectangleOptions}
+                    {selectedPolygon && (
+                        <Polygon
+                            paths={convertCoordinates(selectedPolygon.geometry.coordinates)} // Convert coordinates
+                            options={polygonOptions}
                         />
                     )}
                 </GoogleMap>
             </div>
-            <Insights metaData={metaData} />
         </LoadScript>
     );
 }
