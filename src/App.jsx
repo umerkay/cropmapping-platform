dab420e7f2c0eaa65d3cb69e055803c2631ee8c6
umerkayvyro <muhammad.umer.khan@vyro.ai>
2025-03-07 15:22:51 +0500

    added mui components

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
 import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
 import MapComponent from "./Components/Map";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import Dashboard from "./MUIPages/dashboard/Dashboard";
+import MarketingPage from "./MUIPages/marketing-page/MarketingPage";
+import SignInSide from "./MUIPages/sign-in-side/SignInSide";
 
 function App() {
   const [mode, setMode] = React.useState(localStorage.getItem("mode") || "dark");
+  const [authenticated, setAuthenticated] = React.useState(true); // Static authenticated state
 
   useEffect(() => {
-    localStorage.setItem("mode", mode
-    );
+    localStorage.setItem("mode", mode);
   }, [mode]);
 
   return (
-    <div className={"App " 
-      + (mode === "dark" ? "dark" : "light")
-    }>
-      <MapComponent
-        mode={mode}
-        setMode={setMode}
-      />
-      <ToastContainer/>
+    <div className={"App " + (mode === "dark" ? "dark" : "light")}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MarketingPage />} />
+          <Route path="/signin" element={<SignInSide />} />
+          <Route path="/dashboard" element={authenticated ? <Dashboard /> : <Navigate to="/signin" />} />
+          <Route path="/map" element={authenticated ? <MapComponent mode={mode} setMode={setMode} /> : <Navigate to="/signin" />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+      <ToastContainer />
     </div>
   );
 }
--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,89 +1,103 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { GoogleMap, LoadScript, Polygon } from '@react-google-maps/api';
-import Controls from './Controls';
+import React, { useState, useRef, useEffect } from "react";
+import { GoogleMap, LoadScript, Polygon } from "@react-google-maps/api";
+import Controls from "./Controls";
+import "../Map.scss";
 
 const containerStyle = {
-    width: '100%',
-    height: '100%',
+  width: "100%",
+  height: "100%",
 };
 
 const center = {
-    lat: 30,
-    lng: 70,
+  lat: 30,
+  lng: 70,
 };
 
 const polygonOptions = {
-    fillColor: '#FF0000',
-    fillOpacity: 0,
-    strokeColor: '#FF0000',
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    strokeDasharray: [4, 4] // Makes the border dotted (4px dash, 4px space)
+  fillColor: "#FF0000",
+  fillOpacity: 0,
+  strokeColor: "#FF0000",
+  strokeOpacity: 0.8,
+  strokeWeight: 2,
+  strokeDasharray: [4, 4], // Makes the border dotted (4px dash, 4px space)
 };
 
 function App({ mode, setMode }) {
-    const [polygons, setPolygons] = useState([]);
-    const [selectedPolygon, setSelectedPolygon] = useState(null);
-    const [geojsonData, setGeojsonData] = useState(null);
-    const mapRef = useRef(null);
+  const [polygons, setPolygons] = useState([]);
+  const [selectedPolygon, setSelectedPolygon] = useState(null);
+  const [geojsonData, setGeojsonData] = useState(null);
+  const mapRef = useRef(null);
 
-    // Fetch the GeoJSON data and list of polygons
-    useEffect(() => {
-        fetch(`${import.meta.env.VITE_API_URL}api/geojson`)
-            .then(response => response.json())
-            .then(data => {
-                setPolygons(data.polygons);
-                setGeojsonData(data.geojson);
-            });
-    }, []);
+  // Fetch the GeoJSON data and list of polygons
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}api/geojson`)
+      .then((response) => response.json())
+      .then((data) => {
+        setPolygons(data.polygons);
+        setGeojsonData(data.geojson);
+      });
+  }, []);
 
-    // Handle polygon selection
-    const handlePolygonSelect = (polygonId) => {
-        const selected = geojsonData.features.find(feature => feature.properties.GID_3 === polygonId);
-        console.log(selected.geometry.coordinates);
-        setSelectedPolygon(selected);
-    };
-
-    const convertCoordinates = (coordinates) => {
-        return coordinates[0].map(([lon, lat]) => ({ lat, lng: lon })); // Flip coordinates
-    };
-
-    const flattenCoordinates = (coords) => {
-        return coords.reduce((acc, val) => {
-          if (Array.isArray(val)) {
-            // If the array is nested, recursively flatten
-            return acc.concat(flattenCoordinates(val)); 
-          } else if (Array.isArray(val) && val.length === 2) {
-            // If it's a valid coordinate pair, push it to the result
-            return acc.concat([val]);
-          } else {
-            // Otherwise, ignore invalid values
-            console.error('Invalid coordinate structure:', val);
-            return acc;
-          }
-        }, []);
-      };
-
-    return (
-        <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS}>
-            <Controls polygons={polygons} onPolygonSelect={handlePolygonSelect} />
-            <div id='map'>
-                <GoogleMap
-                    mapContainerStyle={containerStyle}
-                    center={center}
-                    zoom={6}
-                    onLoad={(map) => { mapRef.current = map; }}
-                >
-                    {selectedPolygon && (
-                        <Polygon
-                            paths={convertCoordinates(selectedPolygon.geometry.coordinates)} // Convert coordinates
-                            options={polygonOptions}
-                        />
-                    )}
-                </GoogleMap>
-            </div>
-        </LoadScript>
+  // Handle polygon selection
+  const handlePolygonSelect = (polygonId) => {
+    const selected = geojsonData.features.find(
+      (feature) => feature.properties.GID_3 === polygonId
     );
+    console.log(selected.geometry.coordinates);
+    setSelectedPolygon(selected);
+
+    // Center the map on the selected polygon
+    const bounds = new window.google.maps.LatLngBounds();
+    convertCoordinates(selected.geometry.coordinates).forEach((coord) =>
+      bounds.extend(coord)
+    );
+    mapRef.current.fitBounds(bounds);
+  };
+
+  const convertCoordinates = (coordinates) => {
+    return coordinates[0].map(([lon, lat]) => ({ lat, lng: lon })); // Flip coordinates
+  };
+
+  const flattenCoordinates = (coords) => {
+    return coords.reduce((acc, val) => {
+      if (Array.isArray(val)) {
+        // If the array is nested, recursively flatten
+        return acc.concat(flattenCoordinates(val));
+      } else if (Array.isArray(val) && val.length === 2) {
+        // If it's a valid coordinate pair, push it to the result
+        return acc.concat([val]);
+      } else {
+        // Otherwise, ignore invalid values
+        console.error("Invalid coordinate structure:", val);
+        return acc;
+      }
+    }, []);
+  };
+
+  return (
+    <div className="custom-css">
+      <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS}>
+        <Controls polygons={polygons} onPolygonSelect={handlePolygonSelect} />
+        <div id="map">
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={6}
+            onLoad={(map) => {
+              mapRef.current = map;
+            }}
+          >
+            {selectedPolygon && (
+              <Polygon
+                paths={convertCoordinates(selectedPolygon.geometry.coordinates)} // Convert coordinates
+                options={polygonOptions}
+              />
+            )}
+          </GoogleMap>
+        </div>
+      </LoadScript>
+    </div>
+  );
 }
 
 export default App;
